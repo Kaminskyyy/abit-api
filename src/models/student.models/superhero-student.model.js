d99aec69e30f47969598e169b659c0eb664de44c
Kaminskyyy <thehustaan@gmail.com>
2022-08-05 20:06:49 +0300

    Added pagination to /api/students/superheroes

--- src/models/student.models/superhero-student.model.js
+++ src/models/student.models/superhero-student.model.js
@@ -1,8 +1,8 @@
 const superheroStudentScheama = require('../../db/schemas/student.schemas/superhero-student.schema');
 const Student = require('../student.models/student.model');
 
-const superheroStudent = Student.discriminator('SuperheroStudent', superheroStudentScheama, {
+const SuperheroStudent = Student.discriminator('SuperheroStudent', superheroStudentScheama, {
 	disriminatorKey: 'Superhero',
 });
 
-module.exports = superheroStudent;
+module.exports = SuperheroStudent;
